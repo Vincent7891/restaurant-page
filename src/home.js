c073181a9f79a6cff8561207c03bac766ea92921
Vincent7891 <137709573+Vincent7891@users.noreply.github.com>
2023-09-13 19:48:09 +0100

    add home and nav js

---
+++ src/home.js
@@ -0,0 +1,46 @@
+const main = document.createElement("main");
+
+const mainContainer = document.createElement("div");
+mainContainer.className = "main-container";
+
+const imageContainer = document.createElement("div");
+imageContainer.className = "image-container";
+
+const pizzaImage = document.createElement("img");
+pizzaImage.className = "pizza"; // Fixed here
+pizzaImage.src = "pizza.png";
+pizzaImage.alt = "An image of a delicious Napolitan pizza";
+
+const mainContentWrapper = document.createElement("div");
+mainContentWrapper.className = "main-content-wrapper"; // Fixed here
+
+const h1 = document.createElement("h1");
+const text = document.createTextNode("Delicious Italian Food - Made ");
+const p = document.createElement("p");
+p.className = "fresh";
+p.textContent = "Fresh!";
+
+const span = document.createElement("span");
+span.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget nulla euismod, ullamcorper nunc vel, tristique velit. Integer vitae neque nec sapien convallis ultrices. Praesent auctor massa in mi gravida dignissim. Fusce vel felis vel lorem aliquam tincidunt. Maecenas sit amet volutpat ante.";
+
+const button = document.createElement("button");
+button.className = "order-button";
+button.textContent = "Order Now!";
+
+h1.appendChild(text);
+h1.appendChild(p);
+
+imageContainer.appendChild(pizzaImage);
+
+mainContentWrapper.appendChild(h1);
+mainContentWrapper.appendChild(span);
+mainContentWrapper.appendChild(button);
+
+mainContainer.appendChild(imageContainer);
+mainContainer.appendChild(mainContentWrapper);
+
+main.appendChild(mainContainer);
+
+document.body.appendChild(main);
+
+
---
+++ src/nav.js
@@ -0,0 +1,28 @@
+const header = document.createElement("header");
+const headerContainer = document.createElement("div");
+headerContainer.className = "header-container"
+
+const logoImg = document.createElement("img")
+logoImg.id = "logo"
+logoImg.src = "logo.png"
+logoImg.alt = "The Logo for Pizza Co"
+
+const nav = document.createElement("nav")
+const ul = document.createElement("ul")
+ul.className = "menu";
+
+const menuItems = ["Home", "About", "Contact"]
+menuItems.forEach(item => {
+    const li = document.createElement("li")
+    const a = document.createElement("a");
+    a.textContent = item
+    li.appendChild(a)
+    ul.appendChild(li);
+})
+
+nav.appendChild(ul)
+headerContainer.appendChild(logoImg)
+headerContainer.appendChild(nav)
+header.appendChild(headerContainer)
+
+document.body.appendChild(header);
